67a3546327ecc9ba5e87c4d078ab9eda70ff106f
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2024-09-18 17:13:14 -0300

    feat: adding bubble sort

---
+++ src/algorithms/BubbleSort.java
@@ -0,0 +1,34 @@
+package algorithms;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int[] array = { 64, 34, 25, 12, 22, 11, 90 };
+
+        bubbleSort(array);
+
+        System.out.println("Sorted array");
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static void bubbleSort(int[] array) {
+
+        int n = array.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                // Se o elemento atual for maior que o próximo
+                if (array[j] > array[j + 1]) {
+
+                    // Troca array[j] e array[j + 1]
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
