d83d389ee1cec0e2b595520ce5bebe5d6bea79f0
Rodrigo Coutinho <rodrigocoutinhojj29@gmail.com>
2024-10-13 18:42:48 -0300

    feat: bubble sort another way to implement

--- src/algorithms/BubbleSort.java
+++ src/algorithms/BubbleSort.java
@@ -31,4 +31,32 @@ public class BubbleSort {
             }
         }
     }
+
+    // Another way to implement bubble sort
+
+    public static void main2(String[] args) {
+
+        int[] array = { 64, 34, 25, 12, 22, 11, 90 };
+
+        boolean swapped = true; // flag to check if any swap is done
+        int aux;
+
+        do {
+            swapped = false;
+            for (int i = 0; i < (array.length - 1); i++) {
+                if (array[i] > array[i + 1]) {
+                    aux = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = aux;
+                    swapped = true;
+                }
+            }
+        } while (swapped);
+
+        System.out.println("Sorted array");
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
 }
